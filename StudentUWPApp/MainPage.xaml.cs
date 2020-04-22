eb12ce5d05d9c7848743cbf63bee3e1ee4d9ce40
GlebOlsen <gleb.olsen@gmail.com>
2020-04-22 11:54:08 +0200

    added studen and mainpage
    Jeg fik nogen fejl med git så jeg har lavet  en ny mappe...

--- StudentUWPApp/MainPage.xaml.cs
+++ StudentUWPApp/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,28 @@ namespace StudentUWPApp
         public MainPage()
         {
             this.InitializeComponent();
+
+            Student Student1 = new Student("Gleb");
+            Student Student2 = new Student("John");
+            Student Student3 = new Student("Simon");
+            Student Student4 = new Student("Thais");
+            Student Student5 = new Student("Hassan");
+            Student Student6 = new Student("Jeppe");
+
+            OC_list_Students.Add(Student1);
+            OC_list_Students.Add(Student2);
+            OC_list_Students.Add(Student3);
+            OC_list_Students.Add(Student4);
+            OC_list_Students.Add(Student5);
+            OC_list_Students.Add(Student6);
+
+            ///Commit 3
+            ///commint 3 3.0
+            ///commit 2.0
+
+
+
         }
+        ObservableCollection<Student> OC_list_Students = new ObservableCollection<Student>();
     }
 }
