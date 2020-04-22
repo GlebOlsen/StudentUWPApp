c88be681b9d192c331b5e124f357a49e2225eaf4
GlebOlsen <gleb.olsen@gmail.com>
2020-04-22 12:37:12 +0200

    fixed somethings

---
+++ StudentUWPApp/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StudentUWPApp
+{
+    class Student : INotifyPropertyChanged
+    {
+        private string _name;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Student(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _navn = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
